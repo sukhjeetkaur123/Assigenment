d25a1ba37d036986f25bfc07fa4e99e548e9ea8e
sukhjeetkaur123 <97154128+sukhjeetkaur123@users.noreply.github.com>
2022-09-12 21:26:02 +0530

    Add files via upload

---
+++ rest/getRequestOrangeHRm.java
@@ -0,0 +1,43 @@
+package rest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class getRequestOrangeHRm {
+//    @Test
+//    public void validate_status_code() {
+//        given().
+//                baseUri("https://opensource-demo.orangehrmlive.com").
+//                when().
+//                get("/web/index.php/api/v2/admin/users").   then().
+//                log().all().
+//
+//                List<>
+//                assertThat().
+//                statusCode(200);
+//    }
+
+         WebDriver driver = WebDriverManager.chromedriver().create();
+
+         public void initialize() {
+             driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+             driver.manage().window().maximize();
+             driver.findElement(By.xpath("(//input[@class='oxd-input oxd-input--active'])[1]")).sendKeys("Admin");
+             driver.findElement(By.xpath("(//input[@class='oxd-input oxd-input--active'])[2]")).sendKeys("admin123");
+             driver.findElement(By.xpath("//button[@type='submit']")).click();
+         WebElement ele = driver.findElement(By.xpath("(//div[@role='row'])[31]"));
+         String text = ele.getText();
+         System.out.println("the text is :"+text);
+
+     }
+    public static void main(String[] args) throws InterruptedException {
+    getRequestOrangeHRm gs = new getRequestOrangeHRm();
+         gs.initialize();
+
+}
+}
